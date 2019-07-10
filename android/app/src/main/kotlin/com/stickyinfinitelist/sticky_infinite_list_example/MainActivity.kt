abdae55b018dce25669d06ea4a81532f4306aadc
Denis Beketsky <tatsu.ukraine@gmail.com>
2019-07-10 10:42:15 +0300

    init

---
+++ android/app/src/main/kotlin/com/stickyinfinitelist/sticky_infinite_list_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.stickyinfinitelist.sticky_infinite_list_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
